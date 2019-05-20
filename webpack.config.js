543886301f294332c9972e0ce8730a1e51170fb4
janeplural <mlle.jihshin@gmail.com>
2019-05-20 14:04:58 -0400

    rename public folder to docs

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'docs'),
       filename: 'bundle.js'
     },
     module: {
@@ -39,7 +39,7 @@ module.exports = (env) => {
     ],
     devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
-      contentBase: path.join(__dirname, 'public')
+      contentBase: path.join(__dirname, 'docs')
     },
     mode: 'development'
   };
